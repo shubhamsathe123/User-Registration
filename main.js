afa78b51e6a033f4fc72a8bf0502fa229f8b0352
shubhamsathe123 <shubhamsathe66@gmail.com>
2020-12-25 18:22:18 +0530

    pattern for valid first name

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 console.log("****welcome to user registration form****");
 const util=require("./utility");
+
+const prompt = require('prompt-sync')();
+const fname=prompt("enter your first name");
+util.firstName(fname);
--- utility.js
+++ utility.js
@@ -1,4 +1,18 @@
 class utility{
+    PROMPT = require('prompt-sync')();
+    FIRSTNAME = RegExp(`^[A-Z]{1}[a-z]{2,}$`);
+    firstName = (fname) =>
+    {
+      if(this.FIRSTNAME.test(fname))
+      {
+          console.log(`${fname} is valid first name `)
+      }
+      else
+      {
+        const fname=this.PROMPT("enter your valid first name again");
+        this.firstName(fname);
+      }
+    }
 
 }
 module.exports=new utility;
