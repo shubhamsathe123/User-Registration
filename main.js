d3749bf00cdbe559aa7024ea783502d4095dee1a
shubhamsathe123 <shubhamsathe66@gmail.com>
2020-12-26 17:44:09 +0530

    refactor:updated one

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 console.log("****welcome to user registration form****");
 const util=require("./utility");
-
 const prompt = require('prompt-sync')();
-const fname=prompt("enter your first name");
-util.firstName(fname);
-const lname =prompt("enter your last name");
+
+const fname=prompt("1)enter your first name ");
+util.firstName(fname)
+console.log(`correct format`);
+
+const lname =prompt("2)enter your last name ");
 util.lastName(lname);
-const email =prompt("enter your email");
+console.log(`correct format`);
+
+const email =prompt("3)enter your email ");
 util.emailCheck(email);
-const number =prompt("enter your mobile no");
+console.log(`correct format`);
+
+const number =prompt("4)enter your mobile no ");
 util.mobileCheck(number);
-const pwd =prompt("enter your password");
+console.log(`correct format`);
+
+const pwd =prompt("5)enter your password ");
 util.passwordCheck(pwd);
+console.log(`correct format`);
--- utility.js
+++ utility.js
@@ -4,67 +4,49 @@ class utility{
     LASTNAME = RegExp(`^[A-Z]{1}[a-z]{2,}$`);
     EMAIL=RegExp('^[a-zA-Z0-9]+([._+#][a-zA-Z0-9]+)*@[a-zA-Z1-9]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,3})?$');
     MOBILE=RegExp('^([0-9]{1,3}[ ]+)?[0-9]{10}$');
-    PASSWORD=RegExp("^([A-Z]{1,})*([(@#$*)-])*[a-zA-Z0-9]{6,}$");
-    firstName = (fname) =>
-    {
-      if(this.FIRSTNAME.test(fname))
-      {
-          console.log(`${fname} is valid first name `)
+    PASSWORD=RegExp('^([A-Z]{1,})*([(@#$*)-])*[a-zA-Z0-9]{6,}$');
+    firstName = (fname) =>{
+      if(this.FIRSTNAME.test(fname)){
+        return this.FIRSTNAME.test(fname);
       }
-      else
-      {
-        const fname=this.PROMPT("enter your valid first name again :");
+      else {
+        fname=this.PROMPT("Invalid!! enter your valid first name : ");
         this.firstName(fname);
       }
     }
-
-    lastName = (lname) =>
-    {
-        if(this.LASTNAME.test(lname))
-        {
-            console.log(`${lname} is valid last name `)
+    lastName = (lname) =>{
+        if(this.LASTNAME.test(lname)){
+          return this.LASTNAME.test(lname);
         }
-        else
-        {
-          const lname=this.PROMPT("enter your valid last name again :");
-          this.firstName(lname);
+        else {
+         lname=this.PROMPT("Invalid!! enter your valid last name : ");
+          this.lastName(lname);
         }
     }
-
-    emailCheck = (email) =>
-    {
-        if(this.EMAIL.test(email))
-        {
-            console.log(`${email} is valid email id `)
+    emailCheck = (email) =>{
+        if(this.EMAIL.test(email)){
+            return this.EMAIL.test(email);
         }
-        else
-        {
-          const email=this.PROMPT("enter your valid email again :");
+        else{
+          email=this.PROMPT("Invalid!! enter your valid email: ");
           this.emailCheck(email);
         }
     }
-    mobileCheck = (number) =>
-    {
-        if(this.MOBILE.test(number))
-        {
-            console.log(`${number} is valid mobile no `)
+    mobileCheck = (number) =>{
+        if(this.MOBILE.test(number)){
+          return this.MOBILE.test(number);
         }
-        else
-        {
-          const number=this.PROMPT("enter your valid mobile no again :");
+        else{
+          number=this.PROMPT("Invalid!! enter your valid mobile no: ");
           this.mobileCheck(number);
         }
     }
-
-    passwordCheck = (pwd) =>
-    {
-        if(this.PASSWORD.test(pwd))
-        {
-            console.log(`${pwd} is valid password `)
+    passwordCheck = (pwd) => {
+        if(this.PASSWORD.test(pwd)) {
+          return this.PASSWORD.test(pwd);
         }
-        else
-        {
-          const pwd=this.PROMPT("enter your valid password again :");
+        else {
+          pwd=this.PROMPT("Invalid!! enter your valid password: ");
           this.passwordCheck(pwd);
         }
     }
